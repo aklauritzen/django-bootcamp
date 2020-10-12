4a26c0a1ecf6fc15f641e1a21eaf66fa5c8c5281
redhawkdk <aklauritzen@gmail.com>
2020-10-12 12:16:55 +0200

    Day 3 - Rendering Dynamic HTML

--- bootcamp/urls.py
+++ bootcamp/urls.py
@@ -17,13 +17,16 @@ from django.contrib import admin
 from django.urls import path, re_path # url
 
 from products.views import (
-    home_view, 
+    home_view,
     product_detail_view,
+    product_list_view,
     product_api_detail_view
-) 
+)
+
 
 urlpatterns = [
     path('search/', home_view),
+    path('products/', product_list_view),
     path('products/<int:pk>/', product_detail_view), #pk = primary key
     # path('products/1/', views.product_detail_view),
     re_path(r'api/products/(?P<pk>\d+)/', product_api_detail_view),        
--- products/views.py
+++ products/views.py
@@ -1,19 +1,31 @@
-from django.http import HttpResponse, JsonResponse, Http404
+from django.http import Http404, HttpResponse, JsonResponse
 from django.shortcuts import render
 
 from products.models import Product
 
+
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+    # return HttpResponse("<h1>Hello World</h1>")
+    context = {"name": "Anders"}
+    return render(request, "home.html", context)
 
+
+# Two different approaches "regular" and Json
 def product_detail_view(request, pk):
     try:
         obj = Product.objects.get(pk=pk)
     except Product.DoesNotExist:
         raise Http404 # Renders html page, with HTTP status code 404
 
-    return HttpResponse(f"Product id {obj.pk}")
+    # return HttpResponse(f"Product id {obj.pk}")
+    return render(request, "products/detail.html", {"object": obj})
+
+def product_list_view(request, *args, **kvargs):
+    qs = Product.objects.all()
+    context = {"object_list": qs}
+    return render(request, "products/list.html", context)
+
 
 def product_api_detail_view(request, pk, *args, **kwargs):
     try:
